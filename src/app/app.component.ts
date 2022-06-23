94278290371257e25856ccdfd1367308769122cf
harshal7148 <harshalparab1@gmail.com>
2022-06-23 16:02:17 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RoomSystem';
+
+  constructor(private http:HttpClient){
+  }
+
+  ngOnInit(){
+
+    // let headers = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+    // });
+
+    //let options = { headers: headers};
+
+    this.http.get('http://localhost:4000/owners').subscribe(data=>{
+      console.log(data);   
+    })
+  }
+  
 }
